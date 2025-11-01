348650c71c8d32a0122a68700573a56b48e6d243
Naman Arora <122680076+namanarora2707@users.noreply.github.com>
2025-11-01 18:24:06 +0530

    Daily Questions

---
+++ 2221.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int triangularSum(vector<int>& nums) {
+    int n = nums.size();
+    for (int len = n; len > 1; --len) {
+        for (int i = 0; i < len - 1; ++i) {
+            nums[i] = (nums[i] + nums[i + 1]) % 10;
+        }
+    }
+    return nums[0];
+}
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    vector<int> nums2 = {5};
+    cout << triangularSum(nums) << endl;
+    cout << triangularSum(nums2) << endl;
+    return 0;
+}
